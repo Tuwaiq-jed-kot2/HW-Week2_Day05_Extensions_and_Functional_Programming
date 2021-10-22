9ccdafbc956d4e9d3a3a5dcf7c3807af87964fdb
Qussai <qussai.Saeed.jed.k2@tuwaiq.edu.sa>
2021-10-22 20:31:26 +0300

    Nice

---
+++ src/solution_part1.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val x = "45212212547"
+    x.rever()
+}
+
+fun String.rever() {
+    println(this.reversed())
+
+}
---
+++ src/solution_part2.kt
@@ -0,0 +1,26 @@
+////Q2P1
+//fun main() {
+//    val valuesToAdd = listOf(1, 18, 73, 3, 44, 6, 1, 33, 2, 22, 5, 7, 32, 11, 9, 8, 24)
+//    val list2 = mutableListOf(2, 5, 34, 71, 8, 94, 6, 3, 5, 5, 8, 7)
+//    val finalR = 0
+//    val Sum = valuesToAdd.filter { it >= 2 }
+//                    .flatMap { list2 + it }
+//                     .map { it * 2 }
+//    println(Sum)
+//}
+//
+//
+//
+//
+//
+//    //Q2P2
+////fun main() {
+////    val valuesToAdd = listOf(1, 18, 73, 3, 44, 6, 1, 33, 2, 22, 5, 7, 32, 11, 9, 8, 24)
+////    val list2 = mutableListOf(2,5,4,7,8,9,6,3,5,5,8,7)
+////    var finalResult = 0
+////            valuesToAdd.filter { it >= 7 }
+////                        .map { it * 2 }
+////                            .forEach { finalResult += it }
+////    println(finalResult)
+////}
+////
