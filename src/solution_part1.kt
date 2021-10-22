fe80a7ac3f309f90ea569e9bdce5d44fc58a0b92
Sirin Alkadi <serenalgadi@gmail.com>
2021-10-22 21:54:53 +0300

    solved :)

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -27,7 +27,7 @@ fun main() {
 //Q1
 
 fun String.replacing(){
-    val x = "a+a|b+b|c+c|d+d|e+e|f+f|g+g|h+h|i+i|j+j|k+k|l+l|m+m|n+n|o+o|p+p|q+q|r+r|s+s|t+t|u+u|v+v|x+x|y+y|z+z".toRegex()
+    val x = "aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|xx|yy|zz".toRegex()
     println(this.lowercase().replace(x,"**"))
 }
 
