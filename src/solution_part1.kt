d0380419a252893f33f8601eca8eb8a90a9d171c
bayanalharthy <Bayan.Mastour.jed.k2@tuwaiq.edu.sa>
2021-10-31 12:30:12 +0300

    done

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,14 +1,11 @@
+
 fun main() {
-   val x = listOf(2, 4, 6 ,8)
-   val n = listOf(1,3, 5 ,7)
 
-   val result =x.filter { it>4 }
-                    .map { it*2 }
-                       .flatMap {n+it}
+val number = 345
+    number.rev()
+}
 
-
-
-             println(result)
-
+fun Int.rev() {
+    println(this.toString().reversed().toInt())
 }
 
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,18 +1,28 @@
 fun main() {
 
-var i = 0
+    val x = listOf(2, 4, 6 ,8)
+    val n = listOf(1,3, 5 ,7)
+
+    val result =x.filter { it>4 }
+        .map { it*2 }
+        .flatMap {n+it}
+
+
+
+    println(result)
+
+    var i = 0
     var xi = mutableListOf(1,18,73,3,44,6,13,33, 2, 22,5,7,32, 11,9,8,24)
-                   .filterNot { it < 7 }
-                                      . map { it * 2 }
-                                          .forEach{i += it}
+        .filterNot { it < 7 }
+        . map { it * 2 }
+        .forEach{i += it}
 
 
 
 
     println(i)
 
-
-
-
 }
+
+
 
